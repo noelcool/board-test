c2449bf5a173ef50e80dcd5ed05476b36b549dd1
jungin <57002548+noelcool@users.noreply.github.com>
2025-02-20 22:28:19 +0900

    주석에서 콜론 제거 (#11)

--- src/main/java/noel/example/board/model/dto/BoardPolicyDto.java
+++ src/main/java/noel/example/board/model/dto/BoardPolicyDto.java
@@ -1,9 +1,9 @@
 package noel.example.board.model.dto;
 
 /**
- * @param: isReplyEnabled - 답글 허용 여부
- * @param: isCommentEnabled - 댓글 허용 여부
- * @param: commentPolicy - 댓글 정책
+ * @param isReplyEnabled - 답글 허용 여부
+ * @param isCommentEnabled - 댓글 허용 여부
+ * @param commentPolicy - 댓글 정책
  * */
 public record BoardPolicyDto(
         boolean isReplyEnabled,
@@ -12,10 +12,10 @@ public record BoardPolicyDto(
 ) {
 
     /**
-     * @param: hasPerCommentLimit - 1인 댓글 최대수 지정 여부
-     * @param: hasDailyUserLimit - 1일 1인 댓글 최대 수 지정 여부
-     * @param: maxPerComment - 1인 댓글 최대수
-     * @param: maxDailyPerUser - 1일 1인 댓글 최대 수
+     * @param hasPerCommentLimit - 1인 댓글 최대수 지정 여부
+     * @param hasDailyUserLimit - 1일 1인 댓글 최대 수 지정 여부
+     * @param maxPerComment - 1인 댓글 최대수
+     * @param maxDailyPerUser - 1일 1인 댓글 최대 수
      * */
     public record CommentPolicy(
             boolean hasPerCommentLimit,
--- src/main/java/noel/example/board/web/request/admin/AdminBoardCreateRequest.java
+++ src/main/java/noel/example/board/web/request/admin/AdminBoardCreateRequest.java
@@ -6,11 +6,11 @@ import noel.example.board.model.dto.BoardPolicyDto;
 import java.time.LocalDateTime;
 
 /**
- * @param: title - 제목
- * @param: policy - 댓글/답글 정책
- * @param: startedAt - 시작일
- * @param: endedAt - 종료일, null 인 경우 종료일 없음
- * @param: status - 활성화 상태
+ * @param title - 제목
+ * @param policy - 댓글/답글 정책
+ * @param startedAt - 시작일
+ * @param endedAt - 종료일, null 인 경우 종료일 없음
+ * @param status - 활성화 상태
  * */
 public record AdminBoardCreateRequest(
         String title,
