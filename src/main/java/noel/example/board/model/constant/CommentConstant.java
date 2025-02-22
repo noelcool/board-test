7911e24cfa74bbad5459c9d7d5ba8a0ebcf3d7cd
noel <noelcool0101@gmail.com>
2025-02-22 13:30:10 +0900

    댓글/답글 상태, contant 추가하라

---
+++ src/main/java/noel/example/board/model/constant/CommentConstant.java
@@ -0,0 +1,6 @@
+package noel.example.board.model.constant;
+
+public class CommentConstant {
+
+    public static final String BLIND_COMMENT = "댓글/답글이 차단되었습니다.";
+}
---
+++ src/main/java/noel/example/board/model/type/CommentStatus.java
@@ -0,0 +1,6 @@
+package noel.example.board.model.type;
+
+public enum CommentStatus {
+    NULL, // 상태 없음
+    BLIND, // 차단
+}
