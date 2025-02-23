7d11cce40d4ddc8bc66edf083837d61ede050deb
jungin <57002548+noelcool@users.noreply.github.com>
2025-02-23 11:38:04 +0900

    user resolver 추가 (#13)
    user resolver 추가 (#13)

--- src/main/java/noel/example/board/exception/ErrorCode.java
+++ src/main/java/noel/example/board/exception/ErrorCode.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 public enum ErrorCode {
 
     ADMIN_NOT_FOUND(HttpStatus.NOT_FOUND, "관리자가 존재하지 않습니다"),
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "사용자가 존재하지 않습니다"),
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버에 오류가 발생했습니다")
     ;
 
