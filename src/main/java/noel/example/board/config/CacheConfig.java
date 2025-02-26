0a298a0a45b2dc41237fe2f4bb5feb57d639e2ad
noel <noelcool0101@gmail.com>
2025-02-26 21:51:17 +0900

    redis cache config 추가

---
+++ src/main/java/noel/example/board/config/CacheConfig.java
@@ -0,0 +1,9 @@
+package noel.example.board.config;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableCaching
+public class CacheConfig {
+}
