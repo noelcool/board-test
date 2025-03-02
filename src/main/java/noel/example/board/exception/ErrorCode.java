64bca31d659ded24fb014ce030536fee00709927
noel <noelcool0101@gmail.com>
2025-03-02 12:55:59 +0900

    게시판 에러 코드 추가

--- src/main/java/noel/example/board/exception/ErrorCode.java
+++ src/main/java/noel/example/board/exception/ErrorCode.java
@@ -8,7 +8,10 @@ public enum ErrorCode {
 
     ADMIN_NOT_FOUND(HttpStatus.NOT_FOUND, "관리자가 존재하지 않습니다"),
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "사용자가 존재하지 않습니다"),
-    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버에 오류가 발생했습니다")
+
+
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버에 오류가 발생했습니다"),
+    NON_EXISTENT_BOARD(HttpStatus.NOT_FOUND, "존재하지 않는 게시판입니다.")
     ;
 
     public final HttpStatus httpStatus;
