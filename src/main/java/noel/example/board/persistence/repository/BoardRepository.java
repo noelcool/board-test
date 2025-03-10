3a27253b54b304d316876497c43bee5f51392ba0
noel <noelcool0101@gmail.com>
2025-03-10 23:34:56 +0900

    specification 추가

--- src/main/java/noel/example/board/persistence/repository/BoardRepository.java
+++ src/main/java/noel/example/board/persistence/repository/BoardRepository.java
@@ -2,8 +2,9 @@ package noel.example.board.persistence.repository;
 
 import noel.example.board.persistence.entity.Board;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface BoardRepository extends JpaRepository<Board, Long> {
+public interface BoardRepository extends JpaRepository<Board, Long>, JpaSpecificationExecutor<Board> {
 }
