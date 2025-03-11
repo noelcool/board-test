19b49116a6fb43235f60d3cb90eaf0c8bd44d4e8
noel <noelcool0101@gmail.com>
2025-03-11 22:04:53 +0900

    댓글/답글 엔티티 추가하라

---
+++ src/main/java/noel/example/board/persistence/entity/Comment.java
@@ -0,0 +1,38 @@
+package noel.example.board.persistence.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "comment")
+@RequiredArgsConstructor
+@Getter
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "parentId", nullable = true)
+    private Long parentId;
+
+    @Column(name = "text", nullable = false)
+    private String text;
+
+    @Column(name = "blocked_status", nullable = false)
+    private boolean blockedStatus = false;
+
+    @CreatedBy
+    @Column(name = "created_by", nullable = false, updatable = false)
+    private String createdBy;
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+}
