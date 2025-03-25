df66cba59d27fdc92cc29591e180fe176405d78e
noel <noelcool0101@gmail.com>
2025-03-25 21:58:18 +0900

    코멘트 좋아요 entity, repository 추가

---
+++ src/main/java/noel/example/board/persistence/entity/CommentLike.java
@@ -0,0 +1,43 @@
+package noel.example.board.persistence.entity;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "comment_like")
+@RequiredArgsConstructor
+@Getter
+public class CommentLike {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "comment_id", nullable = false)
+    private Long commentId;
+
+    @CreatedBy
+    @Column(name = "created_no", nullable = false, updatable = false)
+    private Long createdNo;
+
+    @CreatedBy
+    @Column(name = "created_by", nullable = false, updatable = false)
+    private String createdBy;
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @Builder
+    public CommentLike(String createdBy, Long createdNo) {
+        this.createdBy = createdBy;
+        this.createdNo = createdNo;
+    }
+
+}
---
+++ src/main/java/noel/example/board/persistence/repository/CommentLikeRepository.java
@@ -0,0 +1,10 @@
+package noel.example.board.persistence.repository;
+
+import noel.example.board.persistence.entity.CommentLike;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface CommentLikeRepository extends JpaRepository<CommentLike, Long> {
+}
