513f46f8f8ec8b42ba737b720a74b7ca106c16e8
noel <noelcool0101@gmail.com>
2025-03-26 23:31:06 +0900

    게시판 1개의 댓글 목록 조회 추가

--- src/main/java/noel/example/board/persistence/repository/CommentLikeRepository.java
+++ src/main/java/noel/example/board/persistence/repository/CommentLikeRepository.java
@@ -4,6 +4,7 @@ import noel.example.board.persistence.entity.CommentLike;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 
@@ -12,4 +13,5 @@ public interface CommentLikeRepository extends JpaRepository<CommentLike, Long> 
 
     Optional<CommentLike> findByCommentIdAndCreatedNo(Long commentId, Long userNo);
 
+    List<CommentLike> findAllByCommentIdInAndCreatedNo(List<Long> commentIds);
 }
